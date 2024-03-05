9cb91371785f016164aeebda5501b449abfcd2e1
PedroLsRz <pedrodbr955@gmail.com>
2024-03-05 08:45:29 -0300

    first commit

---
+++ php/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link href="../css/index.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar-esquerda">
+        <a class="servicos" href="servicos.php">Serviços</a>
+        <a class="teste" href="servicos.php">Teste</a>
+        <a class="rodape" href="servicos.php">Rodapé</a>
+
+    </nav>
+    <nav class="navbar-direita">Olá
+
+    </nav>
+    <nav class="navbar-secundaria">Olá</nav>
+
+
+
+
+
+
+
+
+</body>
+</html>

090efab5f864c3fa79544eb51bdf1a873636ef32
uhenryl <hsv.rtpb@gmail.com>
2024-03-05 14:48:55 -0300

    henrik-nav-carrossel

--- php/index.php
+++ php/index.php
@@ -8,23 +8,58 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 <body>
-    <nav class="navbar-esquerda">
-        <a class="servicos" href="servicos.php">Serviços</a>
-        <a class="teste" href="servicos.php">Teste</a>
-        <a class="rodape" href="servicos.php">Rodapé</a>
 
-    </nav>
-    <nav class="navbar-direita">Olá
+<body>
+        <header>
+            <a class="logo" href="../php/index.php"><img src="../img/rix-logo-2.png" alt="logo"></a>
+            
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="#">0800 73 20 749
 
-    </nav>
-    <nav class="navbar-secundaria">Olá</nav>
+            <span class="material-symbols-outlined phone-0800">
+            call
+            </span>
+
+            </a>
+            
+            <p class="menu cta">Menu</p>
+        </header>
+        <div class="overlay">
+            <a class="close">&times;</a>
+            <div class="overlay__content">
+                <a href="#">Services</a>
+                <a href="#">Projects</a>
+                <a href="#">About</a>
+            </div>
+        </div>
 
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
+    <section class="container">
+        <div class="slider-wrapper">
+            <div class="slider">
+                <img id="slide-1" src="https://images.unsplash.com/photo-1656464868371-602be27fd4c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" alt="3D rendering of an imaginary orange planet in space" />
+                <img id="slide-2" src="https://images.unsplash.com/photo-1657586640569-4a3d4577328c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" alt="3D rendering of an imaginary green planet in space" />
+                <img id="slide-3" src="https://images.unsplash.com/photo-1656077217715-bdaeb06bd01f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" alt="3D rendering of an imaginary blue planet in space" />
+            </div>
+            <div class="slider-nav">
+                <a href="#slide-1"></a>
+                <a href="#slide-2"></a>
+                <a href="#slide-3"></a>
+            </div>
+        </div>
+    </section>
 
-
-
-
+<div class="buttonzap">
+  <a href='https://wa.me/5583982192221'class="icon" target='_blank'><i class="fab fa-whatsapp"></i></a>
+</div>
 
 </body>
 </html>
